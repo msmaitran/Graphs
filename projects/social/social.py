0481fe3469707f77975c978bdd3dcb982457df30
msmaitran <itsmaisn@gmail.com>
2020-04-15 11:05:50 -0700

    Add solution for populate_graph

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -45,8 +47,27 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        # Use add_user num_users time
 
         # Create friendships
+        for i in range(0, num_users):
+            self.add_user(f"User {i + 1}")
+        
+        # Generate all friendship combinations
+        possible_friendships = []
+
+        # Avoid duplicates by making sure first number is smaller than second
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # Shuffle all possible friendships
+        random.shuffle(possible_friendships)
+
+        # Create for first X pairs is total //2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
